ac63beb8078a86df74206ed8f4918286f0a8603a
Hyun Yi <hy00un__@naver.com>
2023-08-20 14:16:00 +0900

    Add files via upload

---
+++ Vagrantfile
@@ -0,0 +1,18 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.define "vm" do |cfg|
+    cfg.vm.box = "generic/ubuntu2204"
+    cfg.vm.provider "virtualbox" do |vb|
+      vb.name = "vm"
+      vb.cpus = 2
+      vb.memory = 4096
+    end
+    #cfg.vm.host_name = "pwn"
+    cfg.vm.network "forwarded_port", guest:22, host: 31337, auto_correct: true, id: "ssh"
+    cfg.vm.synced_folder ".", "/vagrant", disabled: true
+    cfg.vm.provision "shell", path: "config.sh"
+    cfg.vm.provision "shell", path: "install_pkg.sh"
+  end
+end
---
+++ install_pkg.sh
@@ -0,0 +1,48 @@
+sudo sed -i 's@archive.ubuntu.com@mirror.kakao.com@g' /etc/apt/sources.list
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y gcc gcc-multilib g++ g++-multilib gdb gdb-multiarch
+sudo apt-get install -y libc6-dev-i386 libc6-dbg libncurses5:i386 libstdc++6:i386
+
+# pwntools, capstone-engine, keystone-engine, miasm, radare2
+# https://stackoverflow.com/questions/29310688/sudo-pip-install-vs-pip-install-user
+sudo apt-get install -y python3 python3-pip python3-dev python3-setuptools git libssl-dev libffi-dev build-essential
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
+pip3 install keystone-engine
+pip3 install miasm
+
+git clone https://github.com/radare/radare2
+radare2/sys/install.sh
+
+# angr, z3, pysmt, sympy, archinfo, pyvex, claripy, cle, unicorn-engine, etc..
+pip3 install angr
+
+# ROPGadget, Ropper, rp++, miasm
+pip3 install ropper
+wget https://github.com/0vercl0k/rp/releases/latest/download/rp-lin-gcc.zip
+unzip rp-lin-gcc.zip
+chmod 755 rp-lin
+rm -f rp-lin-gcc.zip
+
+# pwndbg
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+sudo ./setup.sh
+cd ../
+echo "alias gdb='sudo gdb'" >> /home/vagrant/.bashrc
+#source ~/.bashrc
+
+# seccomp-tools, one_gadget
+sudo apt-get install -y gcc ruby-dev
+gem install seccomp-tools
+gem install one_gadget
+
+# libc-database
+# git clone https://github.com/niklasb/libc-database
+# ./libc-database/get ubuntu debian
+
+# etc
+pip3 install requests
+sudo apt-get install -y wget make vim net-tools
