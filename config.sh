96de310110f8e68c9b7f2e31f299133a4d6e98e8
Hyun Yi <hy00un__@naver.com>
2023-08-21 21:13:16 +0900

    add private_network setting

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
