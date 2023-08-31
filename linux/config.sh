b3d6d2355328ffcd59f9dbc4bf8014ccac609a6c
Hyun Yi <hy00un__@naver.com>
2023-08-31 19:27:01 +0900

    delete `config.sh`

--- linux/config.sh
+++
@@ -1,3 +0,0 @@
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
